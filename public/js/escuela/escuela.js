bf84731c13c152907e0e402ade3fdd9efba6682f
unknown <juanjoseesva@gmail.com>
2018-06-03 13:47:23 -0500

    oc

--- public/js/escuela/escuela.js
+++ public/js/escuela/escuela.js
@@ -50,7 +50,14 @@ $(function(){
     let servicioSocialCreditos = $('.servicio_social_creditos', padre).text();
     let actividadesComplementariasCreditos = $('.actividades_complementarias_creditos', padre).text();
 
-    console.log(numeroCampus);
+    for(i = 0; i < numeroCampus; i++) {
+      $('#modalEditarCarrera #checkCampus' + i).prop('checked', false);
+      for(j = 0; j < campuses.length; j++) {
+        if($('#modalEditarCarrera #checkCampus' + i).val() == campuses[j].id) {
+          $('#modalEditarCarrera #checkCampus' + i).prop('checked', true);
+        }
+      }
+    }
 
     $('#carreraId').val(carreraId);
     $('#modalEditarCarrera #imgEditarCarrera').attr('src',imgCarrera);
@@ -62,7 +69,7 @@ $(function(){
     $('#modalEditarCarrera #servicioSocialCreditos').val(servicioSocialCreditos);
     $('#modalEditarCarrera #actividadesComplementariasCreditos').val(actividadesComplementariasCreditos);
     
-    Materialize.updateTextFields()
+    Materialize.updateTextFields();
   });
 
   $("#ruta_imagen").on('change', function (evt) {
--- resources/views/carreras/index.blade.php
+++ resources/views/carreras/index.blade.php
@@ -114,10 +114,12 @@
 
         <h3 style="margin-bottom:25px;">Selecciona los campus en los que estará la carrera</h3>
 
+        <?php $index = 0; ?>
         @foreach($campuses as $campus)
           <div class="row col s12">
-            <input type="checkbox" value="{{$campus->id}}" id="myCheckbox{{$campus->id}}" name="campuses[]" class="filled-in" />
-            <label for="myCheckbox{{$campus->id}}">{{$campus->nombre}}</label>
+            <input type="checkbox" value="{{$campus->id}}" id="checkCampus{{$index}}" name="campuses[]" class="filled-in" />
+            <label for="checkCampus{{$index}}">{{$campus->nombre}}</label>
+            <?php $index++ ?>
           </div>
         @endforeach
 
@@ -198,11 +200,14 @@
 
       <h3 style="margin-bottom:25px;">Selecciona los campus en los que estará la carrera</h3>
 
+      <input type="hidden" value="{{count($campuses)}}" id="numero-campus">
+
+      <?php $index = 0; ?>
       @foreach($campuses as $campus)
         <div class="row col s12">
-          <input type="hidden" value="{{$campus->id}}" id="numero-campus[]">
-          <input type="checkbox" value="{{$campus->id}}" id="myCheckbox{{$campus->id}}" name="campuses[]" class="filled-in" />
-          <label for="myCheckbox{{$campus->id}}">{{$campus->nombre}}</label>
+          <input type="checkbox" value="{{$campus->id}}" id="checkCampus{{$index}}" name="campuses[]" class="filled-in" />
+          <label for="checkCampus{{$index}}">{{$campus->nombre}}</label>
+          <?php $index++; ?>
         </div>
       @endforeach
 
